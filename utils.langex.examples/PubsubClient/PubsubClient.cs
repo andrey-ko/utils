a33f0b3a0d11636d4ede84271f95584986994805
andrey.ko-ws <akolomentsev@machinezone.com>
2015-09-30 19:49:40 -0700

    langex, variants: more examples

---
+++ utils.langex.examples/PubsubClient/PubsubClient.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Threading;
+namespace utils.langex.examples {
+
+	public partial class PubsubClient {
+		public void OnSubscribeError(SubscribeError err) {
+			err.Match(
+				unsolicited: u => {
+					Console.WriteLine($"unsolicited event on channel: '{u.channel}'");
+				},
+				errorOutcome: o => {
+					Console.WriteLine($"subscribe request failed: '{o.request}'");
+				}
+			);
+		}
+
+		public void OnSubscribeRequestFailed(object request) {
+			OnSubscribeError(
+				new SubscribeError.ErrorOutcome(request)
+			);
+		}
+
+		public void OnUnsolicitedError(string channel) {
+			OnSubscribeError(
+				new SubscribeError.Unsolicited(channel)
+			);
+		}
+
+	}
+}
